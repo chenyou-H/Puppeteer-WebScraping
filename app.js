4c7a2b2b8497df6baa568cd9b128c9d5f3c99634
chenyou <chenyouhuang@gmail.com>
2024-02-28 16:54:37 -0500

    Implement adjusted the server condition

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const { runWebScrapping } = require("./webScrapping/puppeteerScraping");
+
 const app = express();
 const port = 3000;
 
-app.get("/api/zillow", (req, res) => {
+app.get("/api/homes", async (req, res) => {
   try {
     const city = req.query.city;
-    const houses = city;
+
+    // Check if the 'city' query parameter is missing
+    if (!city) {
+      return res.status(400).json({ error: 'The "city" query parameter is required' });
+    }
+
+    const houses = await runWebScrapping(city);
     res.json({ houses });
   } catch (error) {
     console.error("Error:", error);
