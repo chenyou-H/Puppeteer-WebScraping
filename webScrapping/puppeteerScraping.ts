cd0a08b6c736521fb989132aca20ac8076c5e4dd
chenyou <chenyouhuang@gmail.com>
2024-02-27 19:20:41 -0500

    try to bypass the robot detect

--- webScrapping/puppeteerScraping.ts
+++ webScrapping/puppeteerScraping.ts
@@ -3,7 +3,7 @@ const puppeteer = require("puppeteer");
 async function runWebScrapping(city) {
   // Launch the browser and open a new blank page
   // const browser = await puppeteer.launch();
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({});
   const context = browser.defaultBrowserContext();
   // Get rid of location request and go to google home page
   await context.overridePermissions("https://www.google.com", ["geolocation"]);
@@ -35,29 +35,6 @@ async function runWebScrapping(city) {
 
 runWebScrapping("london");
 
-// async function lookForHomesIn(page, city) {
-//   let inputSelector = "input[placeholder]"; //locate search box
-
-//   try {
-
-// Function to keep scrolling until the next page button appears
-async function scrollToNextPage(page) {
-  await page.evaluate(async () => {
-    await new Promise((resolve, reject) => {
-      const distance = 1000; // Distance to scroll
-      let totalHeight = 0;
-      const timer = setInterval(() => {
-        window.scrollBy(0, distance);
-        totalHeight += distance;
-        if (totalHeight >= document.body.scrollHeight) {
-          clearInterval(timer);
-          resolve();
-        }
-      }, 200); // Scroll every 200 milliseconds
-    });
-  });
-}
-
 // google search
 async function googleSearch(page) {
   // Navigate the page to a URL
@@ -78,18 +55,37 @@ async function googleSearch(page) {
   await page.waitForNavigation();
 }
 
+// Function to keep scrolling until the next page button appears
+async function scrollToNextPage(page) {
+  await page.evaluate(async () => {
+    await new Promise((resolve, reject) => {
+      const distance = 1000; // Distance to scroll
+      let totalHeight = 0;
+      const timer = setInterval(() => {
+        window.scrollBy(0, distance);
+        totalHeight += distance;
+        if (totalHeight >= document.body.scrollHeight) {
+          clearInterval(timer);
+          resolve();
+        }
+      }, 200); // Scroll every 200 milliseconds
+    });
+  });
+}
+
 // find zillow
 async function findZillowFrom(page) {
-  let zillowLink = "zillow.com";
   let nextPageButtonFound = true;
 
   while (nextPageButtonFound) {
-    const zillowLinkSelector = 'a[href*="zillow.com"]';
+    const zillowLinkSelector = 'a[href*="www.zillow.com"]';
     const zillowLink = await page.$(zillowLinkSelector);
 
     if (zillowLink) {
       await zillowLink.click();
       // Wait for the Zillow page to load
+      await page.waitForNavigation();
+      break;
       await page.waitForSelector("h1");
       // Extract and print the title of the Zillow page
       const zillowTitle = await page.$eval(
@@ -99,7 +95,7 @@ async function findZillowFrom(page) {
       console.log(`Title of the Zillow page: ${zillowTitle}`);
       // await browser.close();
     } else {
-      console.log(`Zillow is not found from current results, scroll down`);
+      console.log(`Zillow is not found from current scope, scroll down`);
       await scrollToNextPage(page);
     }
     // Scroll to trigger loading of more content
@@ -118,10 +114,34 @@ async function findZillowFrom(page) {
 }
 
 async function searchCity(page, city) {
-  await page.waitForNavigation();
   const searchBoxSelector = "input[placeholder]";
   // await page.waitForSelector(searchBoxSelector);
   await page.type(searchBoxSelector, city);
+
   // Press Enter to perform the search
   await page.keyboard.press("Enter");
+  await page.waitForNavigation();
+
+  //skip the zillow question for rent or sale
+  const buttonText = "Skip this question";
+
+  // Use the text selector to find the button with the specified text
+  const buttonSelector = `button ::-p-text("${buttonText}")`;
+  // await page.waitForSelector(buttonSelector);
+
+  // Click the button
+  try {
+    // Wait for the button to be present based on the text selector
+    await page.waitForSelector(buttonSelector, { timeout: 5000 });
+
+    // Click the button
+    await page.click(buttonSelector);
+
+    console.log(`Clicked the button with text: ${buttonText}`);
+  } catch (error) {
+    console.error(`Error: Button with text "${buttonText}" not found.`);
+  }
+
+  // const searchResultsSelector = "#grid-search-results > ul > li";
+  // await page.waitForSelector(searchResultsSelector);
 }
